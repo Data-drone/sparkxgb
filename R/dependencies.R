1610bbfce12cad22e334ad9fc7bbb2dd24268f06
Data-drone <bpl.law@gmail.com>
2021-02-15 22:12:27 +1100

    updating the dependencies again to see if we can get it to pull the correct jar

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,7 +1,9 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
   #if (spark_version > "2.4") 
   spark_version <- "3.0"
-    
+
+  #https://repo1.maven.org/maven2/ml/dmlc/xgboost4j_2.12/1.3.1/xgboost4j_2.12-1.3.1.jar
+  # path to latest
   sparklyr::spark_dependency(
     jars = c(
       system.file(
@@ -9,7 +11,8 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         package = "sparkxgb"
       )
     ),
-    packages = "ml.dmlc:xgboost4j-spark:1.3.1"
+    packages = "ml.dmlc:xgboost4j_2.12:1.3.1",
+    repositories = "https://central.maven.porg/maven2/"
   )
 }
 
