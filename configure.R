b6db280789349b2eb54a72c885e6e5da32f13855
Data-drone <bpl.law@gmail.com>
2021-02-15 22:15:14 +1100

    updating the dependencies again to see if we can get it to pull the correct jar

--- configure.R
+++ configure.R
@@ -4,10 +4,11 @@ library(purrr)
 
 spec <- sparklyr::spark_default_compilation_spec() %>%
   map(function(x) {
-    x$jar_dep <- list.files("internal/xgboost4j-spark", full.names = TRUE) %>% 
+    # update for scala for spark 3
+    x$jar_dep <- list.files("internal/xgboost4j_2.12", full.names = TRUE) %>% 
       map_chr(normalizePath)
     x
   }) %>%
-  keep(~ .x$spark_version >= "2.3.0")
+  keep(~ .x$spark_version >= "3.0.0")
 
 sparklyr::compile_package_jars(spec = spec)
